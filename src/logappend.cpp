5b83593826d5245e0f99c51df204419812c3dfd8
Parrot Live user <ischeetz@protonmail.com>
2025-09-24 17:57:38 +0000

    added to-do list on logappend

--- src/logappend.cpp
+++ src/logappend.cpp
@@ -1,1 +1,34 @@
 
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// Example Queries:
+// logappend -T <timestamp> -K <token> (-E <employee-name> | -G <guest-name>) (-A | -L) [-R <room-id>] <log>
+// logappend -B <file>
+
+
+// List of functions that need implemented:
+// Sanitize Input
+// Parse Query
+// Read Batch File
+// Write to Temp File
+// Convert Temp to Final
+// Check Auth Token
+// Get Timestamp
+
+
+
+int main() {
+  cout << "Hello World!" << endl;
+} 
+
+
+void ParseQuery(){
+    // -T <timestamp>
+    // time is measured in seconds since the gallery opened
+
+
+    // -K <token>
+    // -E <employee>
+}
